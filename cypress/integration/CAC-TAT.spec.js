90452ef5c6ee5c5aad42a6eec6873a25eba5eb13
Luciana Silveira <lucianasilveira.ti@gmail.com>
2023-04-01 20:55:30 -0300

    input radio 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -116,6 +116,18 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .should('have.value', 'feedback')        
             
     })
+
+    it.only('marca cada tipo de atendimento', function(){
+        cy.get('input[type="radio"]')
+        .should('have.length', 3 )
+        .each(function($radio){
+            cy.wrap($radio).check()
+            cy.wrap($radio).should('be.checked')
+
+        })
+                   
+            
+    })
 })
 
 
