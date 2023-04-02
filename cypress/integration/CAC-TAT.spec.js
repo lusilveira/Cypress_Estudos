4addcf642b3e351b4fe76467adb6ba69e56e3459
Luciana Silveira <lucianasilveira.ti@gmail.com>
2023-04-01 21:45:39 -0300

    upload de arquivos com Cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -138,7 +138,18 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .should('not.be.checked')
     })
     
-    
+    it('seleciona um arquivo da pasta fixtures', function(){
+        cy.get('input[type="file"]')
+            .should('not.have.value')
+            .selectFile('./cypress/fixtures/example.json')
+            .should(function($input){
+                console.log($input)
+                    expect($input[0].files[0].name).to.equal('example.json')
+            })
+
+            
+    })
+
 })
 
 
