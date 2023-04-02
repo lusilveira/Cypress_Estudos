311f9aa7eb85ce40d8bd7a7df20af64cc13cfa28
Luciana Silveira <lucianasilveira.ti@gmail.com>
2023-04-02 18:45:35 -0300

    links que abrem em outra aba

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -172,4 +172,20 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             })
     })
 
+
+    it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+        cy.get('#privacy a')
+            .should('have.attr', 'target', '_blank')
+    })
+
+
+
+
+
+
+
+
+
+
+
 })
