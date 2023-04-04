67db79ca56d1cd3c78be68f741fe202013543151
Luciana Silveira <lucianasilveira.ti@gmail.com>
2023-04-04 20:40:06 -0300

    preenche a area de texto usando o comando invoke

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -224,7 +224,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
           .should('not.be.visible')
       })
 
-      it.only('preenche a area de texto usando o comando invoke', function(){
+      it('preenche a area de texto usando o comando invoke', function(){
         const longText = Cypress._.repeat('0123456789',20)
         cy.get('#open-text-area')
             .invoke('val', longText)
