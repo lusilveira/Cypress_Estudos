cf190e773f5c9c6fea5f1c556dbe2fde4d6f2b45
Luciana Silveira <lucianasilveira.ti@gmail.com>
2023-04-04 20:57:09 -0300

    requisição HTTP

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -231,4 +231,16 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .should('have.value', longText)
       })
 
+      it('faz uma requisição HTTP', function(){
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response){
+            //console.log(response)
+            const {status, statusText, body} = response
+            expect(status).to.equal(200)
+            expect(statusText).to.equal('OK')
+            expect(body).to.include('CAC TAT')
+        })
+    })
+    
+
 })
