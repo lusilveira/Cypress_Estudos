7801514e18293204ba3ab6a5f85afcf7c2b3f84f
Luciana Silveira <lucianasilveira.ti@gmail.com>
2023-04-04 21:07:18 -0300

    encontre o gato

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -242,5 +242,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         })
     })
     
-
+    it.only('encontra o gato escondido', function(){
+        cy.get('#cat')
+            .invoke('show')
+            .should('be.visible')
+        cy.get('#title')
+            .invoke('text', 'CAT TAT')
+        cy.get('#subtitle')
+            .invoke('text', 'EU 💛 gatos!')
+        
+    })
 })
